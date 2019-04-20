828a2b4fe508b892f8c1b6e6de474c3fc10d8a70
jsnhall <jwhall.webdev@gmail.com>
2019-04-20 10:50:36 -0700

    updated blog template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
+import { css } from '@emotion/core'
+
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
@@ -14,45 +16,53 @@ class BlogPostTemplate extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title={post.frontmatter.title} description={post.excerpt} />
-        <h1>{post.frontmatter.title}</h1>
-        <small
-          style={{
-            display: `block`,
-            marginBottom: rhythm(1),
-          }}
+        <div
+          css={css`
+            max-width: ${rhythm(20)};
+            margin: auto;
+          `}
         >
-          {post.frontmatter.date}
-        </small>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
+          <h1>{post.frontmatter.title}</h1>
+          <small
+            css={css`
+              display: block;
+              margin-bottom: ${rhythm(1)};
+            `}
+          >
+            {post.frontmatter.date}
+          </small>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          <hr
+            css={css`
+              margin-bottom: ${rhythm(1)};
+            `}
+          />
+          <ul
+            css={css`
+              display: flex;
+              flex-wrap: wrap;
+              justify-content: space-between;
+              list-style: none;
+              padding: 0;
+              margin-left: 0;
+            `}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
       </Layout>
     )
   }
