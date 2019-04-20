c0b43130b0355019133ac870b1810b1346b29f87
jsnhall <jwhall.webdev@gmail.com>
2019-04-19 21:49:04 -0700

    adjusted grid for mobile

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -15,8 +15,12 @@ export default function Masthead() {
           <div
             css={css`
               display: grid;
-              grid-template-columns: 30% 5% 65%;
-              grid-template-rows: 0.3fr 0.3fr .1fr 1fr;
+              grid-template-columns: 1fr 1fr 0.5fr;
+              grid-template-rows: auto auto 0.1fr 1fr;
+              ${media.xs} {
+                grid-template-columns: 30% 5% 65%;
+                grid-template-rows: 0.3fr 0.3fr .1fr 1fr;
+              }
               ${media.sm} {
                 grid-template-rows: 0.5fr 0.5fr .1fr 1fr;
               }
@@ -24,8 +28,14 @@ export default function Masthead() {
           >
             <div
               css={css`
-                grid-row: 1 / 5;
-                align-self: center;
+                grid-row: 1 / 3;
+                grid-column: 1 / 3;
+                align-self: start;
+                ${media.xs} {
+                  align-self: center;
+                  grid-row: 1 / 5;
+                  grid-column: 1;
+                }
               `}
             >
               <Image
@@ -39,11 +49,18 @@ export default function Masthead() {
                 font-family: 'Kiona';
                 font-size: 2em;
                 line-height: 1em;
-                grid-column: 3 / 5;
-                grid-row: 1 / 3;
-                align-self: end;
+                grid-column: 3;
+                grid-row: 1 / 5;
+                align-self: start;
+                writing-mode: vertical-lr;
+                justify-self: end;
                 ${media.xs} {
+                  grid-column: 3 / 5;
+                  grid-row: 1 / 3;
                   font-size: 3em;
+                  writing-mode: lr;
+                  align-self: end;
+                  justify-self: start;
                 }
                 ${media.sm} {
                   font-size: 3.5em;
@@ -76,8 +93,13 @@ export default function Masthead() {
             </h1>
             <p
               css={css`
-                grid-column: 3 / 5;
+                grid-column: 1 / 5;
                 grid-row: 4 / 5;
+                align-self: center;
+                ${media.xs} {
+                  grid-column: 3 / 5;
+                  grid-row: 4 / 5;
+                }
               `}
             >Hello, my name is Jason Hall and I'm a passionate front-end developer located in Scottsdale, Arizona. I enjoy creating aesthetically pleasing websites that are highly performant, accessible and user friendly.</p>
           </div>
