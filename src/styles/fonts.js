8cd311926064d96b145d77832ba9653d5ee1b88f
jsnhall <jwhall.webdev@gmail.com>
2019-04-21 19:33:49 -0700

    trying some things to improve performance

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,12 +1,12 @@
-import Kiona from '../fonts/Kiona-Regular.ttf'
-import KionaBold from '../fonts/Kiona-Bold.ttf'
+import Kiona from '../../static/fonts/Kiona-Regular.ttf'
+import KionaBold from '../../static/fonts/Kiona-Bold.ttf'
 
-import MontserratLight from '../fonts/Montserrat-Light.woff2'
-import MontserratLightItalic from '../fonts/Montserrat-LightItalic.woff2'
-import MontserratSemiBold from '../fonts/Montserrat-SemiBold.woff2'
-import MontserratSemiBoldItalic from '../fonts/Montserrat-SemiBoldItalic.woff2'
-import MontserratBold from '../fonts/Montserrat-Bold.woff2'
-import MontserratBoldItalic from '../fonts/Montserrat-BoldItalic.woff2'
+import MontserratLight from '../../static/fonts/Montserrat-Light.woff2'
+import MontserratLightItalic from '../../static/fonts/Montserrat-LightItalic.woff2'
+import MontserratSemiBold from '../../static/fonts/Montserrat-SemiBold.woff2'
+import MontserratSemiBoldItalic from '../../static/fonts/Montserrat-SemiBoldItalic.woff2'
+import MontserratBold from '../../static/fonts/Montserrat-Bold.woff2'
+import MontserratBoldItalic from '../../static/fonts/Montserrat-BoldItalic.woff2'
 
 export default {
   Kiona,
